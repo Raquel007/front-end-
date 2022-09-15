e1f370d201891a1eb96bc3baf6ca67d748dacab6
Raquel007 <mraquel_cavagnaro@hotmail.com>
2022-09-15 18:14:09 -0300

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { ExperienciaYEducacionComponent } from './components/experiencia-y-educacion/experiencia-y-educacion.component';
+import { IniciarSesionComponent } from './components/iniciar-sesion/iniciar-sesion.component';
+import { LogrosComponent } from './components/logros/logros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    EncabezadoComponent,
+    AptitudesComponent,
+    ExperienciaYEducacionComponent,
+    IniciarSesionComponent,
+    LogrosComponent
   ],
   imports: [
     BrowserModule,
